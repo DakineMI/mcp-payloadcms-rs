648a7bd544ee7f0cadb780ab654d933cf1d22190
DakineMI <jonathan.m.hughes.1@gmail.com>
2025-12-21 17:58:42 -0500

    Remove old schema, SQL, types, and validator implementations to streamline codebase and improve maintainability.
    Signed-off-by: DakineMI <jonathan.m.hughes.1@gmail.com>

--- src_old/cli.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(name =  env!("CARGO_PKG_NAME"))]
-#[command(version = env!("CARGO_PKG_VERSION"))]
-#[command(about = "MCP server for Payload CMS",
-long_about = None)]
-pub struct CommandArguments {}
--- src_old/main.rs
+++
@@ -1,8 +0,0 @@
-use clap::Parser;
-
-use mcp_server_template_rs::{cli, error::ServiceResult, server};
-
-#[tokio::main]
-async fn main() -> ServiceResult<()> {
-    server::start_server(cli::CommandArguments::parse()).await
-}
